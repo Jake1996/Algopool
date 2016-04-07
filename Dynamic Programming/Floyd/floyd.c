dece89ebb31fea5d7931f3d460815f97fa404f8c
nishanths96 <nishanths96@gmail.com>
2016-04-07 06:35:30 +0530

    Update floyd.c

--- Dynamic Programming/Floyd/floyd.c
+++ Dynamic Programming/Floyd/floyd.c
@@ -4,25 +4,25 @@ Output : n X n matrix where i,j the element corresponds to the minimum distance 
 #include<stdlib.h>
 #define MIN(x, y) (((x) > (y)) ? (y) : (x))
 int main() {
-	int n,k,j,i;
-	scanf("%d",&n);
+	int n,k,j,i;			// i, j and k are used as loop variables
+	scanf("%d",&n);			// n denotes number of verticies
 	int **R;
-	R = malloc(sizeof(int*)*n);
+	R = malloc(sizeof(int*)*n);	// R refers to a n X n matrix
 	for(i=0;i<n;i++)
 		R[i]=malloc(sizeof(int)*n);
-	for(i=0;i<n;i++)
+	for(i=0;i<n;i++)			// Input an n X n matrix
 		for(j=0;j<n;j++)
 			scanf("%d",&R[i][j]);
 	for(k=0;k<n;k++)
 		for(i=0;i<n;i++)
 			for(j=0;j<n;j++)
 				R[i][j]=MIN(R[i][j],R[i][k]+R[k][j]);
-	for(i=0;i<n;i++) {
+	for(i=0;i<n;i++) {			// displaying the matrix R
 		for(j=0;j<n;j++)
 			printf("%d ",R[i][j]);
 		printf("\n");
 	}
-	for(i=0;i<n;i++)
+	for(i=0;i<n;i++)		// deallocating the memory that was allocated to R
 		free(R[i]);
 	free(R);
 }
